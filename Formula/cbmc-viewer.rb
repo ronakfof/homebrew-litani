7fff2ca377ca65eea897cc5221dcf17097826671
Ronak Fofaliya <ronakf@amazon.com>
2022-02-18 15:31:22 -0500

    update

--- Formula/cbmc-viewer.rb
+++ Formula/cbmc-viewer.rb
@@ -1,36 +1,35 @@
 class CbmcViewer < Formula
   include Language::Python::Virtualenv
-
-  desc "Scans the output of CBMC and produces a browsable summary of the results."
+  desc "Scans the output of CBMC and produces a browsable summary of the results"
   homepage "https://github.com/awslabs/aws-viewer-for-cbmc"
   url "https://github.com/awslabs/aws-viewer-for-cbmc.git",
       tag:      "viewer-2.9",
       revision: "486af0a066b9034ce6bc7a58cf0ab3d9b25ac964"
   license "Apache-2.0"
 
-  depends_on "universal-ctags"
   depends_on "python3"
+  depends_on "universal-ctags"
 
   resource "Jinja2" do
-      url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
-      sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+    url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
+    sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
   end
 
   resource "MarkupSafe" do
-      url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
-      sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
+    url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
+    sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
   end
 
   resource "voluptuous" do
-      url "https://files.pythonhosted.org/packages/c0/2c/ccbeb25364e3e0c5e4522f13d66e2fc639bb4d4ecdf73be0959552cbecb4/voluptuous-0.12.2.tar.gz"
-      sha256 "4db1ac5079db9249820d49c891cb4660a6f8cae350491210abce741fabf56513"
+    url "https://files.pythonhosted.org/packages/c0/2c/ccbeb25364e3e0c5e4522f13d66e2fc639bb4d4ecdf73be0959552cbecb4/voluptuous-0.12.2.tar.gz"
+    sha256 "4db1ac5079db9249820d49c891cb4660a6f8cae350491210abce741fabf56513"
   end
 
   def install
-      virtualenv_install_with_resources
+    virtualenv_install_with_resources
   end
 
   test do
-      system "which", "cbmc-viewer"
+    system "which", "cbmc-viewer"
   end
 end
